ff1e55f0c62420ccdcc7906c3b92a48055056b14
Tri Ho <106542253+Tristanon@users.noreply.github.com>
2023-08-17 23:01:42 -0600

    fix search function error with thumbnail and remove none option for book not in shelves

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,29 +1,30 @@
 import React from "react"
 
 const Book = ({ book, updateBookShelf }) => {
+  const selectedShelf = book.shelf || "none";
 
-    return (
-        <div className="book">
-            <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:  `url(${book.imageLinks.thumbnail})`,}}></div>
-              <div className="book-shelf-changer">
-                <select defaultValue={book.shelf ? book.shelf : "none"} onChange={(e) => updateBookShelf(book, e.target.value)}>
-                  <option value="none" disabled>
-                    Move to...
-                  </option>
-                  <option value="currentlyReading">
-                    Currently Reading
-                  </option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
-              </div>
+  return (
+      <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:  `url(${book?.imageLinks?.thumbnail})`,}}></div>
+            <div className="book-shelf-changer">
+              <select defaultValue={book.shelf ? book.shelf : "none"} onChange={(e) => updateBookShelf(book, e.target.value)}>
+                <option value="none" disabled>
+                  Move to...
+                </option>
+                <option value="currentlyReading">
+                  Currently Reading
+                </option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                { selectedShelf !== "none"  && <option value="none">None</option>}
+              </select>
             </div>
-            <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.publisher}</div>
-        </div>
-    );
+          </div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.publisher}</div>
+      </div>
+  );
 };
 
 export default Book;
